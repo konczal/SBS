2b291f05cf32b64e521fa798441850270943d305
konczal <45964037+konczal@users.noreply.github.com>
2024-12-10 11:38:14 +0100

    Update and rename Normalize2.sh to FilterAndNormalize.sh

--- 01_snps/FilterAndNormalize.sh
+++ 01_snps/FilterAndNormalize.sh
@@ -1,4 +1,9 @@
+#filter low quality SNPs:
+bcftools_filter -g 5 -O z -o all.population.SBS.variants.FILTERED.vcf.gz -i 'QUAL > 30 && DP > 40 && DP < 250 && MQSB > 0.0001' all.population.SBS.variants.vcf.gz
+
 bcftools norm -d any -m + -o all.population.SBS.variants.FILTERED.NORMALIZED_NORMALIZED2.vcf all.population.SBS.variants.FILTERED.NORMALIZED.vcf.gz
 bgzip all.population.SBS.variants.FILTERED.NORMALIZED_NORMALIZED2.vcf
 tabix all.population.SBS.variants.FILTERED.NORMALIZED_NORMALIZED2.vcf
 bcftools stats all.population.SBS.variants.FILTERED.NORMALIZED_NORMALIZED2.vcf.gz > all.population.SBS.variants.FILTERED.NORMALIZED_NORMALIZED2.vcf.gz.stats
+
+mv ll.population.SBS.variants.FILTERED.NORMALIZED_NORMALIZED2.vcf.gz ll.population.SBS.variants.FNNS.vcf.gz
