d6eb3b3f80372f13d331a07e1ef96d0d56fbd39f
konczal <45964037+konczal@users.noreply.github.com>
2024-12-10 11:51:22 +0100

    Update FilterAndNormalize.sh

--- 01_snps/FilterAndNormalize.sh
+++ 01_snps/FilterAndNormalize.sh
@@ -1,9 +1,6 @@
 #filter low quality SNPs:
 bcftools_filter -g 5 -O z -o all.population.SBS.variants.FILTERED.vcf.gz -i 'QUAL > 30 && DP > 40 && DP < 250 && MQSB > 0.0001' all.population.SBS.variants.vcf.gz
-
-bcftools norm -d any -m + -o all.population.SBS.variants.FILTERED.NORMALIZED_NORMALIZED2.vcf all.population.SBS.variants.FILTERED.NORMALIZED.vcf.gz
-bgzip all.population.SBS.variants.FILTERED.NORMALIZED_NORMALIZED2.vcf
-tabix all.population.SBS.variants.FILTERED.NORMALIZED_NORMALIZED2.vcf
-bcftools stats all.population.SBS.variants.FILTERED.NORMALIZED_NORMALIZED2.vcf.gz > all.population.SBS.variants.FILTERED.NORMALIZED_NORMALIZED2.vcf.gz.stats
-
-mv ll.population.SBS.variants.FILTERED.NORMALIZED_NORMALIZED2.vcf.gz ll.population.SBS.variants.FNNS.vcf.gz
+#normalize:
+bcftools_norm -f /users/fk/mkonczal/projects/SBS/assembly/final/SBS_final.scaffolds.fasta -D -o all.population.SBS.variants.FILTERED.NORMALIZEDrepNewSoft.vcf.gz -m + -O z all.population.SBS.variants.FILTERED.vcf.gz
+#as I had some isses in the past with allele counts calculated by bcftools, I recalculate them here with my own python script:
+zcat all.population.SBS.variants.FILTERED.NORMALIZEDrepNewSoft.vcf.gz | python ReCalculateAC.py | bgzip > all.population.SBS.variants.FNNS.recalcAC.vcf.gz
